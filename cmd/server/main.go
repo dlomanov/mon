6562a836f45a6583cbbc0c4d87b9fedd45680765
Denis Lomanov <dlomanov@protonmail.com>
2024-03-28 19:12:43 +0300

    iter16

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+
 	"github.com/dlomanov/mon/internal/apps/server"
 )
 
 func main() {
 	cfg := getConfig()
+
+	go func() { log.Println(http.ListenAndServe("localhost:6061", nil)) }()
+
 	err := server.Run(cfg)
 	if err != nil {
 		panic(err)
