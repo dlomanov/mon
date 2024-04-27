2a355e88e41b550fd891f5a57be061f9490a47d2
Denis Lomanov <dlomanov@protonmail.com>
2024-04-27 12:38:44 +0300

    add file config

--- cmd/agent/config.go
+++ cmd/agent/config.go
@@ -1,26 +1,114 @@
 package main
 
 import (
+	"embed"
+	"encoding/json"
 	"flag"
+	"fmt"
+	"os"
 	"time"
 
 	"github.com/caarlos0/env/v10"
 	"github.com/dlomanov/mon/internal/apps/agent"
 	"github.com/dlomanov/mon/internal/apps/agent/collector"
 	"github.com/dlomanov/mon/internal/apps/agent/reporter"
+	"gopkg.in/yaml.v2"
 )
 
 type rawConfig struct {
-	Addr           string `env:"ADDRESS"`
-	PollInterval   uint64 `env:"POLL_INTERVAL"`
-	ReportInterval uint64 `env:"REPORT_INTERVAL"`
-	Key            string `env:"KEY"`
-	RateLimit      uint64 `env:"RATE_LIMIT"`
-	LogLevel       string `env:"LOG_LEVEL"`
-	PublicKeyPath  string `env:"CRYPTO_KEY"`
+	Addr           string `json:"addr" env:"ADDRESS"`
+	PollInterval   uint64 `json:"poll_interval" env:"POLL_INTERVAL"`
+	ReportInterval uint64 `json:"report_interval" env:"REPORT_INTERVAL"`
+	Key            string `json:"key" env:"KEY"`
+	RateLimit      uint64 `json:"rate_limit" env:"RATE_LIMIT"`
+	LogLevel       string `json:"log_level" env:"LOG_LEVEL"`
+	PublicKeyPath  string `json:"crypto_key" env:"CRYPTO_KEY"`
+	ConfigPath     string `json:"config" env:"CONFIG"`
 }
 
-func (r rawConfig) toConfig() agent.Config {
+//go:embed config.json
+var configFS embed.FS
+
+func getConfig() agent.Config {
+	raw := rawConfig{}
+	raw.readDefault()
+	raw.readConfig()
+	raw.readFlags()
+	raw.readEnv()
+	raw.print()
+	return raw.toConfig()
+}
+
+func (r *rawConfig) readDefault() {
+	content, err := configFS.ReadFile("config.json")
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(content, &r)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (r *rawConfig) readConfig() {
+	path := new(string)
+	*path = ""
+
+	v := flag.Lookup("c")
+	if v == nil {
+		v = flag.Lookup("config")
+	}
+	if v != nil {
+		*path = v.Value.String()
+	}
+	cp, ok := os.LookupEnv("CONFIG")
+	if ok {
+		*path = cp
+	}
+	if *path == "" {
+		return
+	}
+
+	content, err := os.ReadFile(*path)
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(content, &r)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (r *rawConfig) readFlags() {
+	flag.StringVar(&r.Addr, "a", r.Addr, "server address")
+	flag.Uint64Var(&r.PollInterval, "p", r.PollInterval, "metrics poll interval in seconds")
+	flag.Uint64Var(&r.ReportInterval, "r", r.ReportInterval, "metrics report interval in seconds")
+	flag.StringVar(&r.Key, "k", r.Key, "hashing key")
+	flag.Uint64Var(&r.RateLimit, "l", r.RateLimit, "report rate limit")
+	flag.StringVar(&r.LogLevel, "log_level", r.LogLevel, "log level")
+	flag.StringVar(&r.PublicKeyPath, "crypto-key", r.PublicKeyPath, "public key PEM path")
+	flag.StringVar(&r.ConfigPath, "config", r.ConfigPath, "config path")
+	flag.StringVar(&r.ConfigPath, "c", r.ConfigPath, "config path (shorthand)")
+	flag.Parse()
+}
+
+func (r *rawConfig) readEnv() {
+	err := env.Parse(r)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (r *rawConfig) print() error {
+	cyaml, err := yaml.Marshal(r)
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(cyaml))
+	return nil
+}
+
+func (r *rawConfig) toConfig() agent.Config {
 	return agent.Config{
 		CollectorConfig: collector.Config{
 			PollInterval:   time.Duration(r.PollInterval) * time.Second,
@@ -34,24 +122,4 @@ func (r rawConfig) toConfig() agent.Config {
 		},
 		LogLevel: r.LogLevel,
 	}
-}
-
-func getConfig() agent.Config {
-	raw := rawConfig{}
-
-	flag.StringVar(&raw.Addr, "a", "localhost:8080", "server address")
-	flag.Uint64Var(&raw.PollInterval, "p", 2, "metrics poll interval in seconds")
-	flag.Uint64Var(&raw.ReportInterval, "r", 10, "metrics report interval in seconds")
-	flag.StringVar(&raw.Key, "k", "", "hashing key")
-	flag.Uint64Var(&raw.RateLimit, "l", 2, "report rate limit")
-	flag.StringVar(&raw.LogLevel, "log_level", "info", "log level")
-	flag.StringVar(&raw.PublicKeyPath, "crypto-key", "", "public key PEM path")
-	flag.Parse()
-
-	err := env.Parse(&raw)
-	if err != nil {
-		panic(err)
-	}
-
-	return raw.toConfig()
 }
